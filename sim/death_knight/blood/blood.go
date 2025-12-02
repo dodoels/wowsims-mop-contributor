fbf55c819068e5a6150f82b457989fb1c8d5813f
Adrian Klingen <adrian.klingen@deptagency.com>
2025-12-02 14:35:26 +0100

    Fix OnPetEnable loop

--- sim/death_knight/blood/blood.go
+++ sim/death_knight/blood/blood.go
@@ -107,7 +107,7 @@ func (bdk *BloodDeathKnight) ApplyTalents() {
 		})
 	})
 
-	for _, ghoul := range bdk.GetAllActiveGhoulPets() {
+	for _, ghoul := range bdk.ArmyGhoul {
 		oldOnPetEnable := ghoul.OnPetEnable
 		ghoul.OnPetEnable = func(sim *core.Simulation) {
 			if oldOnPetEnable != nil {
